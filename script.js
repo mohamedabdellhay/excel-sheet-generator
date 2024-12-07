1aacf5f3b58f04da55d148fa11cda1f7edab8eac
Mohamed Abdellhay <mohamedabdellhay1@gmail.com>
2024-12-07 14:02:50 +0200

    use sweetalert.js to display an alert for user

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
+// swal("Hello world!");
+
 let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+  rows = document.getElementsByClassName("rows")[0],
+  columns = document.getElementsByClassName("columns")[0];
+tableExists = false;
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  let rowsNumber = parseInt(rows.value),
+    columnsNumber = parseInt(columns.value);
+  console.log("start table");
+  // check if fields empty
+  if (!rowsNumber || !columnsNumber || rowsNumber < 0 || columnsNumber < 0) {
+    swal("fields must not be empty and greater than 0");
+    return;
+  }
+
+  //   render and generate table
+  table.innerHTML = "";
+  for (let i = 0; i < rowsNumber; i++) {
+    var tableRow = "";
+    for (let j = 0; j < columnsNumber; j++) {
+      tableRow += `<td contenteditable></td>`;
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
-    }
-}
+    table.innerHTML += tableRow;
+  }
+  if (rowsNumber > 0 && columnsNumber > 0) {
+    tableExists = true;
+  }
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+  if (!tableExists) {
+    swal("table not exists");
+    return;
+  }
+  var elt = table;
+  var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+  return dl
+    ? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+    : XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
